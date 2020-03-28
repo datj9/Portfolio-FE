073fcd8100edf244c376047e15ec2805c1950a07
Dat Nguyen <tandat198@gmail.com>
2020-03-29 06:33:26 +0700

    update

--- src/store/modules/users.js
+++ src/store/modules/users.js
@@ -1,45 +1,44 @@
 import axios from "axios";
 const apiURL = "https://api-datng.herokuapp.com";
 const state = {
-	isLoadingg: false,
-	message: null,
-	token: "",
-	isAuthenticated: false
+    isLoadingg: false,
+    message: null,
+    token: "",
+    isAuthenticated: false
 };
 
 const getters = {
-	isLoadingg: state => state.isLoadingg,
-	message: state => state.message,
-	token: state => state.token
+    isLoadingg: state => state.isLoadingg,
+    message: state => state.message,
+    token: state => state.token
 };
 
 const actions = {
-	async signin({ commit }, { email, password }) {
-		try {
-			commit("setLoading", true);
-			const res = await axios.post(`${apiURL}/api/users/signin`, { email, password });
-			const { data: { token } = {} } = res;
-			localStorage.setItem("token", token);
-			commit("setAuthenticate", true);
-			commit("setLoading", false);
-			commit("setMessage", "success");
-		} catch (err) {
-			localStorage.removeItem("token");
-			console.log(err);
-		}
-	}
+    async signin({ commit }, { email, password }) {
+        try {
+            commit("setLoading", true);
+            const res = await axios.post(`${apiURL}/api/users/signin`, { email, password });
+            const { data: { token } = {} } = res;
+            localStorage.setItem("token", token);
+            commit("setAuthenticate", true);
+            commit("setLoading", false);
+            commit("setMessage", "success");
+        } catch (err) {
+            localStorage.removeItem("token");
+        }
+    }
 };
 
 const mutations = {
-	setLoading: (state, isLoadingg) => (state.isLoadingg = isLoadingg),
-	setMessage: (state, message) => (state.message = message),
-	setToken: (state, token) => (state.token = token),
-	setAuthenticate: (state, authenticated) => (state.isAuthenticated = authenticated)
+    setLoading: (state, isLoadingg) => (state.isLoadingg = isLoadingg),
+    setMessage: (state, message) => (state.message = message),
+    setToken: (state, token) => (state.token = token),
+    setAuthenticate: (state, authenticated) => (state.isAuthenticated = authenticated)
 };
 
 export default {
-	state,
-	getters,
-	actions,
-	mutations
+    state,
+    getters,
+    actions,
+    mutations
 };

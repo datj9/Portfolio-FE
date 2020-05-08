a0f6a35ae05172d141b4a533235a4bfc99c1562d
Dat Nguyen <51848355+tandat198@users.noreply.github.com>
2020-05-09 06:26:34 +0700

    Update ContentLoader.vue

--- src/components/ContentLoader.vue
+++ src/components/ContentLoader.vue
@@ -32,11 +32,11 @@ export default {
 </script>
 <style lang="scss">
 svg {
-    width: 30%;
+    width: 28%;
 }
 @media only screen and (max-width: 768px) {
     svg {
-        width: 47%;
+        width: 44%;
     }
 }
 @media only screen and (max-width: 576px) {

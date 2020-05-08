7eed7d62253e4f1aa09c3cffb3aabf4ab1624393
Dat Nguyen <51848355+tandat198@users.noreply.github.com>
2020-05-09 00:06:40 +0700

    Update ContentLoader.vue

--- src/components/ContentLoader.vue
+++ src/components/ContentLoader.vue
@@ -6,10 +6,10 @@
         :primaryColor="primaryColor"
         :secondaryColor="secondaryColor"
     >
-        <rect x="0" y="0" rx="5" ry="5" width="1020" height="450" />
-        <rect x="235" y="470" rx="5" ry="5" width="550" height="70" />
-        <rect x="0" y="575" rx="5" ry="5" width="470" height="80" />
-        <rect x="550" y="575" rx="5" ry="5" width="470" height="80" />
+        <rect x="0" y="0" rx="5" ry="5" width="1060" height="450" />
+        <rect x="235" y="470" rx="5" ry="5" width="590" height="70" />
+        <rect x="0" y="575" rx="5" ry="5" width="480" height="80" />
+        <rect x="580" y="575" rx="5" ry="5" width="480" height="80" />
     </content-loader>
 </template>
 
